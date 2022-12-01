545a564d287809af30910edd1e2a39c220a99669
potoyku <58009448+potoyku@users.noreply.github.com>
2022-12-01 20:49:47 +0700

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("14476409", default=None, cast=int)
-API_HASH = config("8e36763bdeedf326512b5453368474cc", default=None)
-BOT_TOKEN = config("5642701991:AAGfKwNnjQmvrJOjbKudRUhK-DNgOE3A79s", default=None)
-SESSION = config("BAB7_iGwOYYCpHoVuLSWyw70Q8_I6y2sK-Tejv0Trm2nY_JCLaA93pzAz1KjdhvHwtrhFXOQ_fhy8XEW8mI5sxl5dwHill529Skwbnrxp-tr1GlNUpfOy574Av667UPQNJiqjTaYyjEMW8KsstqGN8P-il-MOCGWsbNd4J4UScao-_bRXi79QUmMDcd1ACoE_xnTSR1CvRuG04IHv1-DNEBTi8wXxmkUuO4yH7ybQlXSz1E5TLQdpZTXe6U45gq8R_z3b-euvJsJkFeL_BxQkpKwHzOZZiTGbtI12xgNSt8H6orbagdRcBJK-WrzCnLeeRcojT-HR-zgsk3yDCcShz2yAAAAAUJfRBMA", default=None)
-FORCESUB = config("shopeehaul20221", default=None)
-AUTH = config("5471325958", default=None, cast=int)
+API_ID = 14476409
+API_HASH = "8e36763bdeedf326512b5453368474cc"
+BOT_TOKEN ="5642701991:AAGfKwNnjQmvrJOjbKudRUhK-DNgOE3A79s"
+SESSION = "BAB7_iGwOYYCpHoVuLSWyw70Q8_I6y2sK-Tejv0Trm2nY_JCLaA93pzAz1KjdhvHwtrhFXOQ_fhy8XEW8mI5sxl5dwHill529Skwbnrxp-tr1GlNUpfOy574Av667UPQNJiqjTaYyjEMW8KsstqGN8P-il-MOCGWsbNd4J4UScao-_bRXi79QUmMDcd1ACoE_xnTSR1CvRuG04IHv1-DNEBTi8wXxmkUuO4yH7ybQlXSz1E5TLQdpZTXe6U45gq8R_z3b-euvJsJkFeL_BxQkpKwHzOZZiTGbtI12xgNSt8H6orbagdRcBJK-WrzCnLeeRcojT-HR-zgsk3yDCcShz2yAAAAAUJfRBMA"
+FORCESUB = "shopeehaul20221"
+AUTH = "5471325958"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
